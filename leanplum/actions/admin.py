d7c27717a693c68aa580a650fa6ed244ea431e88
Samuel Clements <sdkclements@gmail.com>
2019-09-04 16:14:03 -0400

    User Attribute Export Implementation

--- leanplum/actions/admin.py
+++ leanplum/actions/admin.py
@@ -23,3 +23,21 @@ class Admin(BaseResource):
         }
 
         self._client.request('POST', 'deleteUser', params)
+
+    def export_user(self, user_id):
+        """
+        https://docs.leanplum.com/reference#get_api-action-exportuser
+
+        * The Export Key is used in place of the regular client_key
+
+        :param user_id: REQUIRED The current user ID
+        :return: Retrieves attributes for the current user. This method requires your data export API clientKey.
+        """
+        if not user_id:
+            raise ValueError("user_id is a required field")
+
+        params = {
+            "userId": user_id
+        }
+
+        self._client.request('GET', 'exportUser', params)
